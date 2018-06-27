c49da9847e872502f369360ebadf733450afe543
Yiyang Zhang <yiyang.zhang@adrastea.wustl.edu>
2018-06-27 15:54:13 -0500

    use intel compiler

--- cmake_intel.sh
+++ cmake_intel.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-CC=icc
-CXX=icpc
-cmake -H. -Bbuild
+
+# run intel compiler config
+source /data/yiyang.zhang/bin/intel/compilers_and_libraries_2018.3.222/linux/bin/compilervars.sh intel64
+
+# intel mpi include and bin
+export PATH="/data/yiyang.zhang/bin/intel/compilers_and_libraries_2018.3.222/linux/mpi/intel64/include:$PATH"
+export PATH="/data/yiyang.zhang/bin/intel/compilers_and_libraries_2018.3.222/linux/mpi/intel64/bin:$PATH"
+
+# user installed packages
+# export PATH="~/.local/bin:$PATH"
+
+cmake -DCMAKE_C_COMPILER=/data/yiyang.zhang/bin/intel/compilers_and_libraries_2018.3.222/linux/bin/intel64/icc \
+-DCMAKE_CXX_COMPILER=/data/yiyang.zhang/bin/intel/compilers_and_libraries_2018.3.222/linux/bin/intel64/icpc \
+-H. -Bbuild
