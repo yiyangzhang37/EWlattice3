d70d546e8b45e17a192c120d72ec7ff07179b535
Yiyang Zhang <jtceleron@live.com>
2018-06-28 11:34:19 -0500

    two bubbles

--- main.cpp
+++ main.cpp
@@ -37,7 +37,8 @@ int main(int argc, char** argv) {
 		//EW_Random_Nucl_LongTimeSpectrum(n_rows, n_cols);
 		//test_FFT3D_r2cc2r();
 
-		EW_CSB_OneBubble(n_rows, n_cols);
+		//EW_CSB_OneBubble(n_rows, n_cols);
+		EW_CSB_TwoBubbles(n_rows, n_cols);
 
 		//auto end = std::chrono::high_resolution_clock::now();
 		//std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end-begin).count() << "ms" << std::endl;
